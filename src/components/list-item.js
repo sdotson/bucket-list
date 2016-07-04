7ae692e3c9bf04189169d44caeaed70529697aa5
sdotson <stu.dotson@gmail.com>
2016-07-04 14:53:07 -0400

    fix list item formatting

--- src/components/list-item.js
+++ src/components/list-item.js
@@ -14,40 +14,40 @@ class ListItem extends Component {
   render() {
     return (
       <li className={this.determineItemClass()} key={this.props.id}>
-      <div className="clearfix">
-      <h4 className="pull-left">{this.props.title}</h4>
-      <div className="btn-group pull-right">
-      <button type="button" className="btn btn-default">
-      <span className="glyphicon glyphicon-remove"></span>
-      </button>
-      <button type="button" className="btn btn-default">
-      <span className="glyphicon glyphicon-ok"></span>
-      </button>
-      <button type="button" className="btn btn-default">
-      <span className="glyphicon glyphicon-pencil"></span>
-      </button>
-      <button type="button" className="btn btn-default" onClick={() => this.props.expandItem(this.props.id) }>
-      <span className={this.determineButtonClass()}></span>
-      </button>
-      </div>
-      </div>
-      <div className="details">
-      <div>
-      <hr />
-      <h5>Description</h5>
-      <p>
-      {this.props.description}
-      </p>
-      <p>
-      Added February 2, 2015.
-      </p>
-      </div>
-      <hr />
-      <h5>Categories</h5>
-      <span className="label label-default">Default</span>
-      <span className="label label-default">Default</span>
-      <span className="label label-default">Default</span>
-      </div>
+        <div className="clearfix">
+          <h4 className="pull-left">{this.props.title}</h4>
+          <div className="btn-group pull-right">
+            <button type="button" className="btn btn-default">
+              <span className="glyphicon glyphicon-remove"></span>
+            </button>
+            <button type="button" className="btn btn-default">
+              <span className="glyphicon glyphicon-ok"></span>
+            </button>
+            <button type="button" className="btn btn-default">
+              <span className="glyphicon glyphicon-pencil"></span>
+            </button>
+            <button type="button" className="btn btn-default" onClick={() => this.props.expandItem(this.props.id) }>
+              <span className={this.determineButtonClass()}></span>
+            </button>
+          </div>
+        </div>
+        <div className="details">
+          <div>
+            <hr />
+            <h5>Description</h5>
+            <p>
+            {this.props.description}
+            </p>
+            <p>
+            Added February 2, 2015.
+            </p>
+          </div>
+          <hr />
+          <h5>Categories</h5>
+          <span className="label label-default">Default</span>
+          <span className="label label-default">Default</span>
+          <span className="label label-default">Default</span>
+        </div>
       </li>
     );
   }
