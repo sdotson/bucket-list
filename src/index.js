0a7b3cb4bc15eb3b48f8b753c4ffa743761dd8f9
sdotson <stu.dotson@gmail.com>
2016-07-28 15:12:20 -0400

    make localhost development less messy

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { Router, useRouterHistory } from 'react-router';
+import { Router, useRouterHistory, browserHistory } from 'react-router';
 import { createHistory } from 'history';
 import promise from 'redux-promise';
 
@@ -12,9 +12,17 @@ import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
-const history = useRouterHistory(createHistory)({
+const hostname = document.location.hostname;
+
+let history;
+
+if (hostname === 'localhost') {
+  history = browserHistory;
+} else {
+  history = useRouterHistory(createHistory)({
     basename: "/bucket-list"
-});
+  });
+}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
