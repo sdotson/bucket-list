18410b10908b66969c18bdecded5d07c3c22f1ed
sdotson <stu.dotson@gmail.com>
2016-07-29 15:50:43 -0400

    rebuild

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
 const webpack = require('webpack');
 
 module.exports = {
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ],
   entry: [
     './src/index.js'
   ],
